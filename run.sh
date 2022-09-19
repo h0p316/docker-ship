889391ab76db9fff9de9f92d6770294560f12b7e
Amod Kumar <amod.kumar@niveussolutions.com>
2022-09-19 17:05:43 +0530

    first code push

---
+++ run.sh
@@ -0,0 +1,32 @@
+# sudo chmod 666 /var/run/docker.sock
+echo "Permission Granted........."
+echo "       .---.       "
+echo "      /     \       "
+echo "      \.@-@./       "
+echo "      / \_/ \       "
+echo "     //  _  \\      "
+echo "    | \     )|_       "
+echo "   / \_>  <_/ \       "
+echo "   \__/ --- \__/      "
+# if()
+
+# SONARCONT=$(docker ps -a |grep "sonarqube"| sed 's/ .*//')
+# ORACONT=$(docker ps -a |grep "oracle"| sed 's/ .*//')
+
+# docker start $SONARCONT
+
+# echo "SonarQube...Started......"
+# echo  "                       (_)"
+# echo  "        --""-------   0/      ^^"
+# echo  ".___...../ /__| |__\ \_/H__,      ^^"
+# echo  "\                        /"
+# echo  "#####^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~\O/~~\Q/~^~^~rr"
+
+# docker start $ORACONT
+# echo "Oracle-DB...Started......"
+# echo  "                       (_)"
+# echo  "        --""-------   0/      ^^"
+# echo  ".___...../ /__| |__\ \_/H__,      ^^"
+# echo  "\                        /"
+# echo  "#####^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~\O/~~\Q/~^~^~rr"
+
